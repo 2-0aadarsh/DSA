0701e91a76b4c4cb08299bc5acd021aea6d6bee7
Aadarsh <aadarsh0811@gmail.com>
2026-05-10 01:58:10 +0530

    first commit on strings problems

---
+++ strings/Level 1/1-reverseString.cpp
@@ -0,0 +1,27 @@
+// 1. Reverse String
+
+#include <iostream>
+using namespace std;
+
+string reverseString(const string &s){
+    // string res = "";
+    // for(int i=s.length()-1; i>=0; i--) {
+    //     res +=s[i];
+    // }
+    // cout<<"Reversed "<< s <<" string is "<<res<<endl;
+    
+    string res = s;
+    int start =0; int end=s.length()-1;
+    
+    while(start<end){
+        swap(res[start++], res[end--]);
+    }
+    cout<<"Reversed "<< s <<" string is "<<res<<endl;
+}
+
+int main() {
+    const string s1 = "Aadarsh is a good boy";
+    // cout << s1 << endl;
+    reverseString(s1);
+    return 0;
+}
---
+++ strings/Level 1/2-countvowels.cpp
@@ -0,0 +1,48 @@
+// 2. Count vowels/consonants
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char *ch) {
+    if(!*ch || *ch == ' ') return false;
+
+    const string vowels = "aeiouAEIOU";
+    for(int i=0; i<vowels.length(); i++) {
+        if(vowels[i] == *ch) return true;
+    }
+    return false;
+}
+
+int countVowels(string &str) {
+    if(str.length() == 0) return 0;
+
+    int count = 0;
+    for(int i=0; i<str.length(); i++) {
+        if(isVowel(&str[i])) count++;
+    }
+    return count;
+}
+
+int consonants(string &str) {
+    if(str.length() == 0) return 0;
+
+    int count = 0;
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z') {
+            if(!isVowel(&str[i]) && str[i] != ' ') count++;
+        }
+    }
+    return count;
+}
+
+
+int main(){
+    string s = "Hello there, this is Aadarsh";
+    cout<<"Number of vowels in string "<<s<<" is "<<countVowels(s)<<endl;
+    cout<<"Number of consonants in string "<<s<<" is "<<consonants(s)<<endl;
+
+    // string s = "";
+    // int ascii = (int)s[0];
+    // cout<<"ASCII value of "<<s<<" is "<<ascii<<endl;
+
+    return 0;
+}
